1015ff4c597f28bcddefeabf756b5f6e57250f22
Anurag Solanki <anurag@vinsol.com>
2012-06-25 12:11:28 +0530

    Few changes after initial review

--- app/models/apn_device.rb
+++ app/models/apn_device.rb
@@ -1,8 +1,17 @@
 class ApnDevice < APN::Device
   attr_accessible :host_name, :pass_key, :token, :app_id
   
-  has_many :machines
+  #### Associations ####
+  has_many :machines, :dependent => :destroy
   
+  #### Validations ####
   validates :host_name, presence: true
 #  validates :token , :uniqueness => true
+
+
+  #### Methods ####
+  def pass_key_in_hash
+    { pass_key: pass_key }
+  end
+    
 end
--- lib/api.rb
+++ lib/api.rb
@@ -1,37 +1,61 @@
 require 'apn_on_rails'
+
 module PushDialer
+  
   class API < Grape::API
   	format :json
   	default_format :json
+  	error_format :json
     prefix 'api'
+    
+    version 'v1', :using => :path
+    
+    rescue_from :all do |e|
+      rack_response({ :message => "rescued from #{e.class.name}" })
+    end
+
+    helpers do
+      # see https://github.com/intridea/grape/wiki/Accessing-parameters-and-headers
+      def authenticate!
+        error!('401 Unauthorized', 401) unless env['HTTP_AUTHORIZATION'] == "HUMPTyVIN-SOlPUsH-DIaLERdUMPTY"
+      end
+    end
+    
     resource 'apn_devices' do
+      # before { authenticate! } # added to each resource for skipping un-authorized access
+
     	#Below method returns the randomly* generated pass_key in valid json format
     	#Scenario : iPhone sends post request for pairing
     	#params[:token]
     	#Optional params => host_name
     	post 'create' do
       	device = ApnDevice.find_by_token(params[:token])
-      	device = ApnDevice.new(:host_name=>params[:host_name], :token=>params[:token], :app_id=>1 ) if !device
-      	device.pass_key = (rand(0.0)*100000).to_i
-      	"{\"pass_key\":#{device.pass_key}}" if device.save
+      	if device
+      	  return true
+      	else
+        	device = ApnDevice.new(:host_name=>params[:host_name], :token=>params[:token], :app_id=>1 )
+        	device.pass_key = (rand(0.0)*100000).to_i
+        	device.pass_key_in_hash if device.save      	  
+    	  end
       end
       
-#Request from mac : sends request to get the iPhone with which it is paired
-#Needed when app launched in MAC
+      #Request from mac : sends request to get the iPhone with which it is paired
+      #Needed when app launched in MAC
       get '/show' do
       	machine = Machine.find_by_mac_address params[:mac_address]
-        ApnDevice.find(machine.apn_device_id) if machine
+        machine.apn_device if machine
       end
       
-    end#resource ApnDevice
-    resource 'machines' do
+    end   #resource ApnDevice
     
-		  #This method returns all machines paired with the iPhone. ie: list of macs--
-		  # --so they can be unpaired
-		  #Request by iPhone params[:token]
+
+    resource 'machines' do
+  	  #This method returns all machines paired with the iPhone. ie: list of macs--
+  	  # --so they can be unpaired
+  	  #Request by iPhone params[:token]
     	get '/show'	do
     		iPhone = ApnDevice.find_by_token(params[:token])
-    		Machine.where(:apn_device_id => iPhone.id) if iPhone
+    		iPhone.machines if iPhone
     	end
     	
 #    	Mac sends request to pair with an iPhone. 
@@ -48,23 +72,25 @@ module PushDialer
 					end
 				else
 #					send error message to MAC for invalid pass_key
+          error!({ 'error' => 'invalid pass_key', 'params' => params[:pass_key] }, 401)
 				end
 			end
 			
 			#unpairing from MAC Or iPhone
 			delete do
-		  	if params[:token] #unpairing from iPhone
+		  	if params[:token]   #unpairing from iPhone
 		  		iPhone = ApnDevice.find_by_token params[:token]
 		  		if iPhone && Machine.where(:apn_device_id => iPhone.id).first #machine belongs to that iPhone from which request came 
 		  			Machine.find_by_mac_address(params[:mac_address]).destroy
+		  			## send list of leftout machines paired with this iPhone (json)
 		  		end
-		  	else
+		  	else  #unpairing from MAC
 		  		Machine.find_by_mac_address(params[:mac_address]).destroy
 		  		#Push notification to iPhone
 		  	end
 		  end
 		  
-		end#resource machine
+		end #resource Machine
     
   end #Class
 end #Module
