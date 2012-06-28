59d548ef86f05a4d1439609d0328aa591b3d1df1
Anurag Solanki <anurag@vinsol.com>
2012-06-28 09:39:06 +0530

    Static home page added

--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,7 @@ mount PushDialer::API => "/"
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'static#index'
 
   # See how all your routes lay out with "rake routes"
 
