e68386477c4cab75303754e68ef63892cc1dc2b2
Anurag Solanki <anurag@vinsol.com>
2012-06-28 17:59:11 +0530

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ require "bundler/capistrano"
 
 set :scm,             :git
 set :repository,      "git@github.com:ShubhamGupta/push_dialer.git"
-set :branch,          "origin/master"
+set :branch,          "master"
 set :migrate_target,  :current
 set :ssh_options,     { :forward_agent => true }
 set :rails_env,       "production"
@@ -39,13 +39,15 @@ default_environment["RAILS_ENV"] = 'production'
 # default_environment["RUBY_VERSION"] = "ruby-1.9.2-p290"
 default_environment['PATH'] = "/usr/local/bin:/usr/bin:/bin:/opt/bin:$PATH"
 
+# after 'deploy:finalize_update', 'deploy:make_new_symlink'
+
 namespace :deploy do
   desc "Deploy your application"
   task :default do
     update
     restart
   end
-
+  
   desc "Setup your git-based deployment app"
   task :setup, :except => { :no_release => true } do
     dirs = [deploy_to, shared_path]
@@ -53,24 +55,24 @@ namespace :deploy do
     run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
     run "git clone #{repository} #{current_path}"
   end
-
+  
   task :cold do
     update
     migrate
   end
-
+  
   task :update do
     transaction do
       update_code
     end
   end
-
+  
   desc "Update the deployed code."
   task :update_code, :except => { :no_release => true } do
     run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
     finalize_update
   end
-
+  
   desc "Update the database (overwritten to avoid symlink)"
   task :migrations do
     transaction do
@@ -79,10 +81,10 @@ namespace :deploy do
     migrate
     restart
   end
-
+  
   task :finalize_update, :except => { :no_release => true } do
     run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
-
+  
     # mkdir -p is making sure that the directories are there for some SCM's that don't save empty folders
     run <<-CMD
       rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
@@ -93,13 +95,25 @@ namespace :deploy do
       ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
       ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml
     CMD
-
+  
     if fetch(:normalize_asset_timestamps, true)
       stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
       asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
       run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
     end
   end
+  
+  # task :make_new_symlink do
+  #   run <<-CMD
+  #     rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
+  #     mkdir -p #{latest_release}/public &&
+  #     mkdir -p #{latest_release}/tmp &&
+  #     ln -s #{shared_path}/log #{latest_release}/log &&
+  #     ln -s #{shared_path}/system #{latest_release}/public/system &&
+  #     ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
+  #     ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml
+  #   CMD
+  # end
 
   desc "Zero-downtime restart of Unicorn"
   task :restart, :except => { :no_release => true } do
@@ -123,12 +137,12 @@ namespace :deploy do
       set :branch, "HEAD@{1}"
       deploy.default
     end
-
+  
     desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
     task :cleanup, :except => { :no_release => true } do
       run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
     end
-
+  
     desc "Rolls back to the previously deployed version."
     task :default do
       rollback.repo
