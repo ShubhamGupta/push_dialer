b098acbade54187abab02e2e312fa130cbd3e4e5
Anurag Solanki <anurag@vinsol.com>
2012-06-28 16:06:37 +0530

    Exceptions handled.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  rescue_from Exception, :with => :redirect_to_home
+  
+  def redirect_to_home
+    redirect_to root_path
+  end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -61,4 +61,7 @@ PushDialer::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  
+  # Last line: Any other routes are handled here (as ActionDispatch prevents RoutingError from hitting ApplicationController::rescue_action).
+  match '*a', :to => 'application#redirect_to_home'
 end
