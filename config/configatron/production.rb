ab1dea5029b491a28950490988bfe810f064237f
Anurag Solanki <anurag@vinsol.com>
2012-07-02 09:33:04 +0530

    APNS production certificate included

--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -4,9 +4,11 @@
 #   configatron.file.storage = :s3
 
 # production (delivery):
+  configatron.apn.passphrase = 'pushchat'
   configatron.apn.host = 'gateway.push.apple.com'
   configatron.apn.cert = File.join(Rails.root, 'config', 'apple_push_notification_production.pem')
 
 # production (feedback):
+  configatron.apn.passphrase = 'pushchat'
   configatron.apn.feedback.host = 'feedback.push.apple.com'
   configatron.apn.feedback.cert = File.join(Rails.root, 'config', 'apple_push_notification_production.pem')
