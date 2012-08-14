0dd04cf5269783ab5b84ba93fa9fbcda235a7dc6
Anurag Solanki <anurag@vinsol.com>
2012-08-14 09:54:09 +0530

    Updated unicorn.rb for monit

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,38 +1,3 @@
-# # config/unicorn.rb
-# # Set environment to development unless something else is specified
-# env = ENV["RAILS_ENV"] || "development"
-# 
-# # See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete documentation.
-# worker_processes 4
-# 
-# shared_path = "/home/deploy/apps/push_dialer/shared"
-# 
-# # listen on both a Unix domain socket and a TCP port,
-# # we use a shorter backlog for quicker failover when busy
-# listen "#{shared_path}/tmp/sockets/push_dialer.sock", :backlog => 64
-# 
-# # Preload our app for more speed
-# preload_app true
-# 
-# # nuke workers after 30 seconds instead of 60 seconds (the default)
-# timeout 30
-# 
-# pid "#{shared_path}/pids/unicorn.pid" #"/tmp/unicorn.push_dialer.pid"
-# 
-# # Production specific settings
-# if env == "production"
-#   # Help ensure your application will always spawn in the symlinked
-#   # "current" directory that Capistrano sets up.
-#   working_directory "/home/deploy/apps/push_dialer/current"
-# 
-#   # feel free to point this anywhere accessible on the filesystem
-#   user 'deploy', 'staff'
-#   shared_path = "/home/deploy/apps/push_dialer/shared"
-# 
-#   stderr_path "#{shared_path}/log/unicorn.stderr.log"
-#   stdout_path "#{shared_path}/log/unicorn.stdout.log"
-# end
-
 # config/unicorn.rb
 # Set environment to development unless something else is specified
 env = ENV["RAILS_ENV"] || "development"
@@ -82,7 +47,7 @@ before_fork do |server, worker|
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
+      puts "Old master already dead"    # someone else did our job for us
     end
   end
 end
@@ -98,4 +63,7 @@ after_fork do |server, worker|
   # and Redis.  TokyoCabinet file handles are safe to reuse
   # between any number of forked children (assuming your kernel
   # correctly implements pread()/pwrite() system calls)
+  
+  child_pid = server.config[:pid].sub('.pid', ".#{worker.nr}.pid")
+    system("echo #{Process.pid} > #{child_pid}")
 end
